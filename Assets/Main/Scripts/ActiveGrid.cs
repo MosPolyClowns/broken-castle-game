7e193c45722ed2674312f1a1e3c99457e42d85be
Av0ry1 <Chesnokovae08@icloud.com>
2024-11-10 19:04:16 +0300

    Core mechanics #1000
    Add more levels & traps
    Remake background

--- Assets/Main/Scripts/ActiveGrid.cs
+++ Assets/Main/Scripts/ActiveGrid.cs
@@ -8,21 +8,49 @@ public class ActiveGrid : MonoBehaviour
     [SerializeField] private Collider2D _collider2D;
     [SerializeField] private ParticleSystem _particleSystem;
 
+    [Header("Type")]
+    [SerializeField] private string _type = "Fake";
+
     [Header("Fake")]
     [SerializeField] private TileBase _defaultTileBase;
     [SerializeField] private TileBase _fakeTileBase;
+
+    [Header("Move")]
+    [SerializeField] private Vector2 _targetVelocity;
+
+    private bool _activated = false;
 
     private Vector3 velocity = Vector3.zero;
 
     public void Activate()
     {
-        Fake();
+        if (_activated == false)
+        {
+            _activated = true;
+
+            switch (_type)
+            {
+                case "Fake":
+                    Fake();
+                    break;
+                case "Move":
+                    Move();
+                    break;
+            }
+            
+        }
     }
 
     private void Fake()
     {
         _tilemap.SwapTile(_defaultTileBase, _fakeTileBase);
         _collider2D.enabled = false;
+        _particleSystem.Play();
+    }
+
+    private void Move()
+    {
+        velocity = new Vector3(_targetVelocity.x, _targetVelocity.y, 0);
         _particleSystem.Play();
     }
 
